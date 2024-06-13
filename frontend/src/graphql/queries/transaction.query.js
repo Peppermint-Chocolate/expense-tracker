d4e37b3b32b82c19484b972b61743d5f49ae0902
Peppermint-Chocolate <yinuoqin01194@gmail.com>
2024-06-13 00:20:09 -0400

    implement CRUD transactions

--- frontend/src/graphql/queries/transaction.query.js
+++ frontend/src/graphql/queries/transaction.query.js
@@ -1,1 +1,30 @@
 // Apollo Documentation on Queries: https://www.apollographql.com/docs/react/data/queries
+import { gql } from "@apollo/client"; 
+
+export const GET_TRANSACTIONS = gql` 
+    query GetTransactions {
+        transactions {
+            _id 
+            description 
+            paymentType 
+            category 
+            amount 
+            location 
+            date 
+        }
+    }
+`; 
+
+export const GET_TRANSACTION = gql` 
+    query GetTransaction($id: ID!) {
+        transaction(transactionId: $id) {
+            _id 
+            description 
+            paymentType 
+            category 
+            amount 
+            location 
+            date 
+        }
+    }
+`; 
--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -1,24 +1,48 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-// import { GET_TRANSACTION, GET_TRANSACTION_STATISTICS } from "../graphql/queries/transaction.query";
-// import { UPDATE_TRANSACTION } from "../graphql/mutations/transcation.mutation";
+import { GET_TRANSACTION } from "../graphql/queries/transaction.query";
+import { UPDATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
 import toast from "react-hot-toast";
-// import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
+import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
 
 const TransactionPage = () => {
+	const { id } = useParams();
+	const { loading, data } = useQuery(GET_TRANSACTION, {
+		variables: { id: id },
+	});
+
+	const [updateTransaction, { loading: loadingUpdate }] = useMutation(UPDATE_TRANSACTION);  
+
+	console.log("Transaction", data);
+
 	const [formData, setFormData] = useState({
-		description: "",
-		paymentType: "",
-		category: "",
-		amount: "",
-		location: "",
-		date: "",
+		description: data?.transaction?.description || "",
+		paymentType: data?.transaction?.paymentType || "",
+		category: data?.transaction?.category || "",
+		amount: data?.transaction?.amount || "", 
+		location: data?.transaction?.location || "",
+		date: data?.transaction?.date || "",
 	});
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		console.log("formData", formData);
+		const amount = parseFloat(formData.amount); // convert amount to number because by default it is string 
+													// and the reason is it comes from an input field 
+		try {
+			await updateTransaction({
+				variables: {
+                    input: {
+                        ...formData, 
+						amount, 
+						transactionId: id, 
+                    },
+                },
+			})
+			toast.success("Transaction updated successfully"); 
+		} catch (error) {
+			toast.error("Failed to update transaction: ", error.message); 
+		}
 	};
 	const handleInputChange = (e) => {
 		const { name, value } = e.target;
@@ -28,7 +52,20 @@ const TransactionPage = () => {
 		}));
 	};
 
-	// if (loading) return <TransactionFormSkeleton />;
+	useEffect(() => { 
+		if (data) { 
+			setFormData({
+                description: data?.transaction?.description,
+                paymentType: data?.transaction?.paymentType,
+                category: data?.transaction?.category,
+                amount: data?.transaction?.amount, 
+                location: data?.transaction?.location,
+                date: new Date(+data.transaction.date).toISOString().substr(0, 10),
+            });
+        }
+	}, [data])
+
+	if (loading) return <TransactionFormSkeleton />;
 
 	return (
 		<div className='h-screen max-w-4xl mx-auto flex flex-col items-center'>
@@ -182,8 +219,9 @@ const TransactionPage = () => {
 					className='text-white font-bold w-full rounded px-4 py-2 bg-gradient-to-br
           from-pink-500 to-pink-500 hover:from-pink-600 hover:to-pink-600'
 					type='submit'
+					disabled={loadingUpdate}
 				>
-					Update Transaction
+					{ loadingUpdate? 'Updating...' : 'Update Transaction' }
 				</button>
 			</form>
 		</div>
